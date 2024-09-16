365197455595c00d24898c33bcffdb3ac5df7215
Suncica Babic <63728473+suncic@users.noreply.github.com>
2024-09-16 08:41:21 +0200

    Add files via upload

---
+++ RadSaFajlovima/RadSaFajlovima/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadSaFajlovima
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            //ispisivanje celog fajla linije po linije
+
+            string file = @"E:\VSprojekti\RadSaFajlovima\RadSaFajlovima\file.txt";
+            using (StreamReader sr = new StreamReader(file))
+            {
+                string linija = null;
+                while ((linija = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(linija);
+                }
+            }
+
+            Console.WriteLine("\n2.test\n");
+
+            //ispisuje sve u jednom redu sve jedno pored drugog
+
+            using (StreamReader sr = new StreamReader(file))
+            {
+                StringBuilder sb = new StringBuilder();
+                string linija = null;
+                while ((linija = sr.ReadLine()) != null)
+                {
+                    sb.Append(linija.Trim());
+                }
+
+                Console.WriteLine(sb.ToString());
+            }
+
+            Console.WriteLine("\n3.test\n");
+
+            //svaka linija u poseban red(bas sadrzaj fajla) - splitovanje po redu
+
+            using (StreamReader sr = new StreamReader(file))
+            {
+                StringBuilder sb = new StringBuilder();
+                string linija = null;
+                while ((linija = sr.ReadLine()) != null)
+                {
+                    sb.AppendLine(linija);
+                }
+
+                Console.WriteLine(sb.ToString());
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RadSaFajlovima/RadSaFajlovima/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RadSaFajlovima")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RadSaFajlovima")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2e58177c-8f27-4e10-943e-e4e6fa2b40ca")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
